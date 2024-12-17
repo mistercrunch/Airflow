69bf622fdba02d4bbfce4d9385e1da9d19b85fe1
jx2lee <dev.jaejun.lee.1991@gmail.com>
2024-12-18 00:45:22 +0900

    generated datamodel-codegen

--- task_sdk/src/airflow/sdk/api/datamodels/_generated.py
+++ task_sdk/src/airflow/sdk/api/datamodels/_generated.py
@@ -202,6 +202,9 @@ class VariableResponse(BaseModel):
     Variable schema for responses with fields that are needed for Runtime.
     """
 
+    model_config = ConfigDict(
+        extra="forbid",
+    )
     key: Annotated[str, Field(title="Key")]
     value: Annotated[str | None, Field(title="Value")] = None
 
@@ -211,6 +214,9 @@ class XComResponse(BaseModel):
     XCom schema for responses with fields that are needed for Runtime.
     """
 
+    model_config = ConfigDict(
+        extra="forbid",
+    )
     key: Annotated[str, Field(title="Key")]
     value: Annotated[Any, Field(title="Value")]
 
@@ -260,6 +266,9 @@ class TIRunContext(BaseModel):
     Response schema for TaskInstance run context.
     """
 
+    model_config = ConfigDict(
+        extra="forbid",
+    )
     dag_run: DagRun
     max_tries: Annotated[int, Field(title="Max Tries")]
     variables: Annotated[list[VariableResponse] | None, Field(title="Variables")] = None
