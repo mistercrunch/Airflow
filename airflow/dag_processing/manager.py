4b3f066a4b491f03a1935309b57ea40cab210e4d
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-01-21 19:35:56 +0100

    AIP-66: Make DAG callbacks bundle aware
    This involves using relative paths in the callbacks, resolving the full path and using the
    it to queue the callback in the file processor process.
    
    Don't serialize dag if running callback
    
    Refactor codes and fix tests
    
    update callback test
    
    Fix typing
    
    Don't use relative fileloc yet
    
    Add test
    
    apply suggestions from code review
    
    Fix some tests and require bundle_version in DagCallbackRequest
    
    Resolve conflicts and fix tests
    
    join load dagversion as part of TI in process executor event

--- airflow/dag_processing/manager.py
+++ airflow/dag_processing/manager.py
@@ -46,6 +46,7 @@ from uuid6 import uuid7
 
 import airflow.models
 from airflow.configuration import conf
+from airflow.dag_processing.bundles.manager import DagBundlesManager
 from airflow.dag_processing.collection import update_dag_parsing_results_in_db
 from airflow.dag_processing.processor import DagFileParsingResult, DagFileProcessorProcess
 from airflow.exceptions import AirflowException
@@ -105,6 +106,7 @@ class DagFileInfo:
     rel_path: Path
     bundle_name: str
     bundle_path: Path | None = field(compare=False, default=None)
+    bundle_version: str | None = None
 
     @property
     def absolute_path(self) -> Path:
@@ -230,8 +232,6 @@ class DagFileProcessorManager:
         # TODO: AIP-66 move to report by bundle self.log.info(
         #     "Checking for new files in %s every %s seconds", self._dag_directory, self.dag_dir_list_interval
         # )
-
-        from airflow.dag_processing.bundles.manager import DagBundlesManager
 
         DagBundlesManager().sync_bundles_to_db()
 
@@ -409,15 +409,22 @@ class DagFileProcessorManager:
 
     def _add_callback_to_queue(self, request: CallbackRequest):
         self.log.debug("Queuing %s CallbackRequest: %s", type(request).__name__, request)
-        file_info = DagFileInfo(path=request.full_filepath, bundle_name=request.bundle_name)
+        try:
+            bundle = DagBundlesManager().get_bundle(name=request.bundle_name, version=request.bundle_version)
+        except ValueError:
+            # Bundle no longer configured
+            self.log.error("Bundle %s no longer configured, skipping callback", request.bundle_name)
+            return None
+
+        dag_absolute_path = os.fspath(Path(bundle.path, request.filepath))
+
+        file_info = DagFileInfo(
+            path=dag_absolute_path,
+            bundle_path=bundle.path,
+            bundle_name=request.bundle_name,
+            bundle_version=request.bundle_version,
+        )
         self._callback_to_execute[file_info].append(request)
-        if file_info in self._file_path_queue:
-            # Remove file paths matching request.full_filepath from self._file_path_queue
-            # Since we are already going to use that filepath to run callback,
-            # there is no need to have same file path again in the queue
-            self._file_path_queue = deque(
-                file_path for file_path in self._file_path_queue if file_path != file_info
-            )
         self._add_paths_to_queue([file_info], True)
         Stats.incr("dag_processing.other_callback_count")
 
@@ -678,13 +685,7 @@ class DagFileProcessorManager:
         """
         self._file_paths = new_file_paths
 
-        # clean up the queues; remove anything queued which no longer in the list, including callbacks
-        self._file_path_queue = deque(x for x in self._file_path_queue if x in new_file_paths)
         Stats.gauge("dag_processing.file_path_queue_size", len(self._file_path_queue))
-
-        callback_paths_to_del = [x for x in self._callback_to_execute if x not in new_file_paths]
-        for path_to_del in callback_paths_to_del:
-            del self._callback_to_execute[path_to_del]
 
         # Stop processors that are working on deleted files
         filtered_processors = {}
